497700d41d0e3c84b33aa53d057b79d8b8c865de
RahulS2905 <rahulsswamy@gmail.com>
2026-05-13 22:59:35 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+// first create myapp folder and then create two files Helloworld.java and Docker file 
+
+  public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from Docker!");
+    }
+}
